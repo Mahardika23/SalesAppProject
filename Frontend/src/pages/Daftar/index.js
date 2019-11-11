4d5c559dde5badef132045adae5767c9b8e8548e
randycahya99 <randycahya99@gmail.com>
2019-11-11 11:56:26 +0700

    Validation Form

--- Frontend/src/pages/Daftar/index.js
+++ Frontend/src/pages/Daftar/index.js
@@ -3,92 +3,205 @@ import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap
 import "./Daftar.css";
 import {Link} from 'react-router-dom'
 import logo from '../../../src/logo.svg'
-import axios from 'axios';
-class Daftar extends React.Component{
 
-handleChange = event => {
-    this.setState({ 
-      Username: event.target.value,
-      });
+/*const initialState = {
+  Username: "",
+  Password: "",
+  Konfirmasi: "",
+  UsernameError: "",
+  PasswordError: "",
+  KonfirmasiError: ""
+}
+
+export default class ValidationForm extends React.Component {
+  state = initialState;
+
+  handleChange = event => {
+    const isCheckbox = event.target.type === "checkbox";
+    this.setState({
+      [event.target.name]: isCheckbox
+        ? event.target.name.checked
+        : event.target.value
+    });
+  };
+
+  validate = () => {
+    let UsernameError: "";
+    let PasswordError: "";
+    let KonfirmasiError: "";
+
+    //Username
+    if (!this.state.Username) {
+      UsernameError = "*username tidak boleh dikosongkan";
+    }
+
+    else if (this.state.Username !== null) {
+      if (this.state.Username.match(/^[a-zA-Z]/ || /^[0-9]/)) {
+        UsernameError = "";
+      }
+      else {
+        UsernameError = "tidak dapat menggunakan karakter spesial";
+      }
+    }
+
+    //Password
+    if(!this.state.Password) {
+      PasswordError = "*passwrord tidak boleh dikosongkan";
+    }
+
+    //Konfirmasi
+    if (!this.state.Konfirmasi) {
+      KonfirmasiError = "*tidak boleh dikosongkan";
+    }
+
+    if (UsernameError || PasswordError || KonfirmasiError) {
+      this.setState({UsernameError, PasswordError, KonfirmasiError});
+      return false;
+    }
+
+    return true;
   }
-handleChangeEmail = event => {
-    this.setState({ 
-      email: event.target.value,
-      });
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const isValid = this.validate();
+    if (isValid) {
+      console.log(this.state);
+      this.setState(initialState);
+    }
   }
-  handleChangePass = event => {
-    this.setState({ 
-      Password: event.target.value,
+
+  onSubmitButton = () => {
+    if (this.validate) {
+      return window.location = "./daftar2";
+    }
+  }*/
+
+class Register extends React.Component {
+  constructor () {
+    super ();
+    this.state = {
+      fields: {},
+      errors: {}
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.submitRegis = this.submitRegis.bind(this);
+  };
+
+  handleChange (e) {
+    let fields = this.state.fields;
+    fields[e.target.name] = e.target.value;
+    this.setState ({
+      fields
     });
   }
-    handleChangePassConfirm = event => {
-    this.setState({ 
-      Password: event.target.value,
-      Konfirmasi: event.target.value,
+
+  submitRegis (e) {
+    e.preventDefault ();
+    if (this.validateForm()) {
+      let fields = {};
+      fields["Username"] = "";
+      fields["Password"] = "";
+      fields["Konfirmasi"] = "";
+      this.setState ({fields:fields});
+    }
+  }
+
+  validateForm () {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    //Username
+    if (!fields["Username"]) {
+      formIsValid = false;
+      errors["Username"] = "*username wajib diisi";
+    }
+
+    if (typeof fields["Username"] !== "undefined") {
+      if (!fields["Username"].match(/^[a-zA-Z]*$/ || /^[0-9]*$/)) {
+        formIsValid = false;
+        errors["Username"] = "*tidak dapat menggunakan karakter khusus";
+      }
+    }
+
+    //Password
+    if (!fields["Password"]) {
+      formIsValid = false;
+      errors["Password"] = "*password wajib diisi";
+    }
+
+    if (typeof fields["Password"] !== "undefined") {
+      if (!fields["Passowrd"].match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
+        formIsValid = false;
+        errors["Password"] = "*password kurang aman";
+      }
+    }
+
+    //Konfirmasi Password
+    if (!fields["Konfirmasi"]) {
+      formIsValid = false;
+      errors["Konfirmasi"] = "*kolom ini wajib diisi";
+    }
+
+    if (typeof fields["Konfirmasi"] !== "undefined") {
+      if (fields["Konfirmasi"] !== fields["Password"]) {
+        formIsValid = false;
+        errors["Konfirmasi"] = "*password tidak cocok";
+      }
+    }
+
+    this.setState ({
+      errors:errors
     });
-  }
-handleSubmit = async () => {
-    // event.preventDefault();
-
-    const user = {
-      email:this.state.email,
-      name: this.state.Username,
-      password: this.state.Password,
-      password_confirmation: this.state.Konfirmasi
-    };
-
-
-      let res = await axios.post('http://127.0.0.1:8000/api/register',  user );
-      // let {data} = res.data;
-      // this.setState({user:data})
-      // .then(res => {
-      //   // console.log(res);
-          console.log(user);
-        
-      // })
-    
+    return formIsValid;
   }
 
-render(){
 
-  return (
-    <div className='halaman'>
-      <div style={{backgroundColor:'white'}}className="daftar">
-          <h1 ><b>SALES APP</b></h1>
-          <img src={logo} alt="Logo" width="300px"></img>
-          <h2>Daftar</h2>
-      <Form className="container" onSubmit={this.handleSubmit}>  
-        <FormGroup row>
-          <Label for="exampleSelect" sm={3}>Username</Label>
-          <Col sm={9}>
-            <Input type="text" name="Username" id="exampleSelect" onChange={this.handleChange}/>
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="exampleSelect" sm={3}>Email</Label>
-          <Col sm={9}>
-            <Input type="email" name="email" id="exampleSelect" onChange={this.handleChangeEmail}/>
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="examplePassword" sm={3}>Password</Label>
-          <Col sm={9}>
-            <Input type="password" name="Password" id="examplePassword"  onChange={this.handleChangePass}/>
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="exampleSelect" sm={3}>Konfirmasi</Label>
-          <Col sm={9}>
-            <Input type="password" name="Konfirmasi" id="exampleSelect" onChange={this.handleChangePassConfirm}/>
-          </Col>
-        </FormGroup>
-       <Button variant ="purple"  type="submit">
-            Selanjutnya
+  render () {
+    return (
+      <div className='halaman'>
+        <div style={{backgroundColor:'white'}}className="daftar">
+            <h1 ><b>SALES APP</b></h1>
+            <img src={logo} alt="Logo" width="300px"></img>
+            <h2>Daftar</h2>
+        <Form method="post" className="container" onSubmit={this.submitRegis}>  
+          <FormGroup row>
+            <Label for="exampleSelect" sm={3}>Username</Label>
+            <Col sm={9}>
+              <Input type="text" name="Username" id="exampleSelect" value={this.state.fields.Username} onChange={this.handleChange} />
+              <div style={{fontSize: 12, color: "red"}}>
+                {this.state.errors.Username}
+              </div>
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="examplePassword" sm={3}>Password</Label>
+            <Col sm={9}>
+              <Input type="password" name="Password" id="examplePassword" value={this.state.fields.Password} onChange={this.handleChange} />
+              <div style={{fontSize: 12, color: "red"}}>
+                {this.state.errors.Password}
+              </div>
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="exampleSelect" sm={3}>Konfirmasi</Label>
+            <Col sm={9}>
+              <Input type="password" name="Konfirmasi" id="exampleSelect" value={this.state.fields.Password} onChange={this.handleChange} />
+              <div style={{fontSize: 12, color: "red"}}>
+                {this.state.errors.Password}
+              </div>
+            </Col>
+          </FormGroup>
+          <Button variant ="purple"  type="submit">
+              Selanjutnya
           </Button>
-      </Form>
+        </Form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-}
-export default Daftar;
+
+export default Register;
--- Frontend/src/pages/Daftar2/index.js
+++ Frontend/src/pages/Daftar2/index.js
@@ -5,51 +5,178 @@ import {Link} from 'react-router-dom'
 import logo from '../../../src/logo.svg'
 
 
-const Daftar2 = (props) => {
-  return (
-    <div className='halaman'>
-      <div style={{backgroundColor:'white'}}className="daftar">
-          <h1 ><b>SALES APP</b></h1>
-          <img src={logo} alt="Logo" width="300px"></img>
-          <h2>Daftar</h2>
-      <Form className="container">  
-        <FormGroup row>
-          <Label for="exampleEmail" sm={3}>Nama Toko</Label>
-          <Col sm={9}>
-            <Input type="text" name="Namatoko" id="exampleEmail" />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="examplePassword" sm={3}>Alamat</Label>
-          <Col sm={9}>
-            <Input type="text" name="Alamat" id="examplePassword"  />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="exampleSelect" sm={3}>Nama Pemilik</Label>
-          <Col sm={9}>
-            <Input type="text" name="Pemilik" id="exampleSelect" />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="exampleSelectMulti" sm={3}>Email</Label>
-          <Col sm={9}>
-            <Input type="email" name="Email" id="exampleSelectMulti"  />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="examplePassword" sm={3}>No Telp</Label>
-          <Col sm={9}>
-            <Input type="text" name="NoTelp" id="examplePassword"  />
-          </Col>
-        </FormGroup>
-        <Link to='./login'><Button variant ="purple"  type="submit">
-            Daftar
-          </Button></Link>
-      </Form>
+class Register2 extends React.Component {
+  constructor () {
+    super ();
+    this.state = {
+      fields: {},
+      errors: {}
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitRegis2 = this.submitRegis2.bind(this);
+  };
+  
+  handleChange (e) {
+    let fields = this.state.fields;
+    fields[e.target.name] = e.target.value;
+    this.setState ({
+      fields
+    });
+  }
+
+  submitRegis2 (e) {
+    e.preventDefault ();
+    if (this.validateForm()) {
+      let fields = {};
+      fields["Namatoko"] = "";
+      fields["Alamat"] = "";
+      fields["Pemilik"] = "";
+      fields["Email"] = "";
+      fields["NoTelp"] = "";
+      this.setState ({fields:fields});
+      alert ("Data berhasil dikirim");
+    }
+  }
+
+  validateForm () {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsvalid = true;
+
+    //Nama Toko
+    if (!fields["Namatoko"]) {
+      formIsvalid = false;
+      errors["Namatoko"] = "*nama toko harus diisi";
+    }
+
+    if (typeof fields["Namatoko"] !== "undefined") {
+      if (!fields["Namatoko"].match(/^[a-zA-Z]*$/ || /^[0-1]/)) {
+        formIsvalid = false;
+        errors["Namatoko"] = "*tidak dapat menggunakan karakter khusus"
+      }
+    }
+
+    //Alamat
+    if (!fields["Alamat"]) {
+      formIsvalid = false;
+      errors["Alamat"] = "*alamat harus diisi";
+    }
+
+    if (typeof fields["Alamat"] !== "undefined") {
+      if (!fields["Alamat"].match(/^[a-zA-Z]*$/)) {
+        formIsvalid = false;
+        errors["Alamat"] = "*tidak dapat menggunakan karakter khusus"
+      }
+    }
+
+    //Nama Pemilik
+    if (!fields["Pemilik"]) {
+      formIsvalid = false;
+      errors["Pemilik"] = "*nama pemilik harus diisi";
+    }
+
+    if (typeof fields["Pemilik"] !== "undefined") {
+      if (!fields["Pemilik"].match(/^[a-zA-Z]*$/)) {
+        formIsvalid = false;
+        errors["Pemilik"] = "*tidak dapat menggunakan karakter khusus";
+      }
+    }
+
+    //E - mail
+    if (!fields["Email"]) {
+      formIsvalid = false;
+      errors["Email"] = "*e-mail harus diisi";
+    }
+
+    if (typeof fields["Email"] !== "undefined") {
+      var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+      if (!pattern.test(fields["Email"])) {
+        formIsvalid = false;
+        errors["Email"] = "*e-mail tidak valid";
+      }
+    }
+
+    //No. Telepon 
+    if (!fields["NoTelp"]) {
+      formIsvalid = false;
+      errors["NoTelp"] = "*no. telepon harap diisi";
+    }
+
+    if (typeof fields["NoTelp"] !== "undefined") {
+      if (!fields["NoTelp"].match(/^[0-9]{12}$/)) {
+        formIsvalid = false;
+        errors["NoTelp"] = "no. telepon tidak valid"
+      }
+    }
+
+    this.setState ({
+      errors:errors
+    });
+    return formIsvalid;
+  }
+
+
+  render () {
+    return (
+      <div className='halaman'>
+        <div style={{backgroundColor:'white'}}className="daftar">
+            <h1 ><b>SALES APP</b></h1>
+            <img src={logo} alt="Logo" width="300px"></img>
+            <h2>Daftar</h2>
+        <Form method="post" className="container" onSubmit={this.submitRegis2}>  
+          <FormGroup row>
+            <Label for="exampleEmail" sm={3}>Nama Toko</Label>
+            <Col sm={9}>
+              <Input type="text" name="Namatoko" id="exampleEmail" value={this.state.fields.Namatoko} onChange={this.handleChange} />
+              <div style={{fontSize: 12, color: "red"}}>
+                {this.state.errors.Namatoko}
+              </div>
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="examplePassword" sm={3}>Alamat</Label>
+            <Col sm={9}>
+              <Input type="text" name="Alamat" id="examplePassword" value={this.state.fields.Alamat} onChange={this.handleChange} />
+              <div style={{fontSize: 12, color: "red"}}>
+                {this.state.errors.Alamat}
+              </div>
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="exampleSelect" sm={3}>Nama Pemilik</Label>
+            <Col sm={9}>
+              <Input type="text" name="Pemilik" id="exampleSelect" value={this.state.fields.Pemilik} onChange={this.handleChange} />
+              <div style={{fontSize: 12, color: "red"}}>
+                {this.state.errors.Pemilik}
+              </div>
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="exampleSelectMulti" sm={3}>Email</Label>
+            <Col sm={9}>
+              <Input type="email" name="Email" id="exampleSelectMulti" value={this.state.fields.Email} onChange={this.handleChange} />
+              <div style={{fontSize: 12, color: "red"}}>
+                {this.state.errors.Email}
+              </div>
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="examplePassword" sm={3}>No Telp</Label>
+            <Col sm={9}>
+              <Input type="text" name="NoTelp" id="examplePassword" value={this.state.fields.NoTelp} onChange={this.handleChange} />
+              <div style={{fontSize: 12, color: "red"}}>
+                {this.state.errors.NoTelp}
+              </div>
+            </Col>
+          </FormGroup>
+          <Button variant ="purple"  type="submit">
+              Daftar
+          </Button>
+        </Form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default Daftar2;
+export default Register2;
