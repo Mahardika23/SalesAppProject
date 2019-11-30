5457aa162b6bcffeff991596d02f77fd84129732
HilmyFI <hilmy.fadhla@gmail.com>
2019-11-30 18:52:35 +0700

    add login toko and data flow

--- Frontend_new/Web Customer/resources/views/daftar2.blade.php
+++ Frontend_new/Web Customer/resources/views/daftar2.blade.php
@@ -57,31 +57,39 @@
                         </div>
                         <!-- /.Horizontal Steppers -->
                        
-                        <form action="/daftar/alamat" method="get" style='text-align:left'>
+                        <form action="/daftar/alamat" method="POST" style='text-align:left'>
+                        @CSRF
                         <div style='margin-left:10%;margin-right:10% '>
+                            <input type="text" name="username" id="username" value="{{$input['username']}}">
+                            <input type="text" name="email" id="email" value="{{$input['email']}}">
+                            <input type="text" name="password" id="password" value="{{$input['password']}}">
                             <div class="form-group row">
-                                <label for="nama_toko" class="col-sm-3 col-form-label">Nama Toko</label>
+                                <label for="nama_toko" class="col-sm-3 col-form-label">Nama Toko </label>
                                 <div class="col-sm-9">
-                                <input type="text" class="form-control" id="nama_toko" placeholder="" autocomplete='off'>
+                                <input type="text" name="nama_toko" class="form-control" id="nama_toko" placeholder="" autocomplete='off'>
+                                <span class='text-danger'>{{ $errors->first('nama_toko') }}</span>
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="nama_pemilik" class="col-sm-3 col-form-label">Nama Pemilik</label>
                                 <div class="col-sm-9">
-                                <input type="text" class="form-control" id="nama_pemilik" placeholder="" autocomplete='off'>
+                                <input type="text" name="nama_pemilik" class="form-control" id="nama_pemilik" placeholder="" autocomplete='off'>
+                                <span class='text-danger'>{{ $errors->first('nama_pemilik') }}</span>
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="email_pemilik" class="col-sm-3 col-form-label">Email Pemilik</label>
                                 <div class="col-sm-9">
-                                <input type="email" class="form-control" id="email_pemilik" placeholder="" autocomplete='off'>
+                                <input type="email" name="email_pemilik" class="form-control" id="email_pemilik" placeholder="" autocomplete='off'>
+                                <span class='text-danger'>{{ $errors->first('email_pemilik') }}</span>
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="no_telp" class="col-sm-3 col-form-label">No Telp</label>
                                 <div class="col-sm-9">
-                                <input type="text" class="form-control" id="no_telp" placeholder="" autocomplete='off'>
-                                </div>
+                                <input type="text" name="no_telp" class="form-control" id="no_telp" placeholder="" autocomplete='off'>
+                                <span class='text-danger'>{{ $errors->first('no_telp') }}</span>    
+                            </div>
                             </div>
                         </div>
                         <div class="form-group" style="text-align:RIGHT; margin-right:9%">
