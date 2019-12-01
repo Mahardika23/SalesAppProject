bdfee0dc12a19a5f0ec00943f2939d353e1aa1bb
HilmyFI <hilmy.fadhla@gmail.com>
2019-12-01 13:39:16 +0700

    add Login Daftar sent data

--- Frontend_new/Web Customer/resources/views/daftar2.blade.php
+++ Frontend_new/Web Customer/resources/views/daftar2.blade.php
@@ -10,10 +10,6 @@
 </head>
 
 <body style="background: linear-gradient(0deg, #E4DFEC 70%, #403151 60%); ">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="{{ url('/js/mdb.min.js')}}"></script>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-8">
@@ -60,9 +56,10 @@
                         <form action="/daftar/alamat" method="POST" style='text-align:left'>
                         @CSRF
                         <div style='margin-left:10%;margin-right:10% '>
-                            <input type="text" name="username" id="username" value="{{$input['username']}}">
-                            <input type="text" name="email" id="email" value="{{$input['email']}}">
-                            <input type="text" name="password" id="password" value="{{$input['password']}}">
+                            <input type="hidden" name="name" id="name" value="{{$input['name']}}">
+                            <input type="hidden" name="email" id="email" value="{{$input['email']}}">
+                            <input type="hidden" name="password" id="password" value="{{$input['password']}}">
+                            <input type="hidden" name="password_confirmation" id="password_confirmation" value="{{$input['password_confirmation']}}">
                             <div class="form-group row">
                                 <label for="nama_toko" class="col-sm-3 col-form-label">Nama Toko </label>
                                 <div class="col-sm-9">
@@ -101,4 +98,8 @@
             </div>
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="{{ url('/js/mdb.min.js')}}"></script>
 </body>
