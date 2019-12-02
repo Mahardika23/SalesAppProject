48c4b33bd6e612694f65f90f4052312faad1c61b
Arya Gamas <aryagamas@gmail.com>
2019-12-03 01:58:10 +0700

    add search barang API

--- Backend/app/Http/Controllers/Api/CatalogController.php
+++ Backend/app/Http/Controllers/Api/CatalogController.php
@@ -8,35 +8,57 @@ use App\Distributor;
 use App\Barang;
 use JWTAuth;
 use App\User;
+
 class CatalogController extends Controller
 {
-    public function showall(){
+    public function showall()
+    {
         // $distributor = 
         return $barang = Barang::all();
-
     }
-    public function showalltoWeb(){
+    public function showalltoWeb()
+    {
         // $distributor = 
         return $barang = Barang::paginate(12);
     }
-    
-    public function showByFilter(Request $request){
-        
+
+    public function showByFilter(Request $request)
+    {
+
         // $id = Distributor::find(1)->where('province_id','11')->pluck('id');
         return $barang = Barang::find(1)
-        ->join('distributors','barangs.distributor_id','=','distributors.id')
-        ->select('barangs.*','distributors.nama_distributor')->where('distributors.regency_id',$request['regency_id'])
-        /*->orWhere('distributors.regency_id',$request['regency_id'])*/->get();
+            ->join('distributors', 'barangs.distributor_id', '=', 'distributors.id')
+            ->select('barangs.*', 'distributors.nama_distributor')->where('distributors.regency_id', $request['regency_id'])
+            /*->orWhere('distributors.regency_id',$request['regency_id'])*/->get();
         // return $barang = Barang::all()->where('distributor_id',['11','12']);
 
         Barang::allowFilter()->get();
     }
-    public function showByUser(Request $request){
+    public function showByUser(Request $request)
+    {
         $user = JWTAuth::parseToken()->authenticate();
         $userToko = User::find($user['id'])->userable->regency_id;
         return $barang = Barang::find(1)
-        ->join('distributors','barangs.distributor_id','=','distributors.id')
-        ->select('barangs.*','distributors.nama_distributor')->where('distributors.regency_id',$userToko)
-       ->get();
+            ->join('distributors', 'barangs.distributor_id', '=', 'distributors.id')
+            ->select('barangs.*', 'distributors.nama_distributor')->where('distributors.regency_id', $userToko)
+            ->get();
+    }
+
+    public function searchBy(Request $request)
+    {
+        
+        $barang = Barang::with('distributor')->where('nama_barang','LIKE', '%'.$request['search'].'%')
+        ->orWhereHas('distributor',function($query){
+            global $request;
+            $query->where('nama_distributor','LIKE',$request['search']);
+        })->paginate(10);
+        return $barang;
+         
+
+        /*
+        $posts = App\Post::whereHas('comments', function ($query) {
+        $query->where('content', 'like', 'foo%');
+        })->get();
+        */
     }
 }
